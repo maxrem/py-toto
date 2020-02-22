b597765d77f3ce82e262bca8a091b253919c3288
Max R <max.remkes@gmail.com>
2020-02-22 20:01:55 +0100

    Fix flask host-, redis connect issue

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,6 @@ from flask_redis import FlaskRedis
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
@@ -15,13 +14,10 @@ def create_app(test_config=None):
     redis_client = FlaskRedis(app=app)
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -32,7 +28,6 @@ def create_app(test_config=None):
     def hello():
         redis_client.sadd('test', 'test value')
 
-        return redis_client.get('test')
-        # return os.environ['REDIS_URL']
+        return redis_client.spop('test')
 
     return app
