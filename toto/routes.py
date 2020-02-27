db52e2590f831cad88465d1d8cb8febb29997043
Max Remkes <max@pbwebmedia.com>
2020-02-27 16:48:42 +0100

    Chapter 2: Templates

--- toto/routes.py
+++ toto/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
+
 from toto import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Max'}
+    posts = [
+        {
+            'author': {'username': 'Henk'},
+            'body': 'Henkie penkie'
+        },
+        {
+            'author': {'username': 'Jan'},
+            'body': 'Jantje Piet'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
