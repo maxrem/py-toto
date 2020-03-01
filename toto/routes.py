dd086ebf3f44ecdfabbf94b56263c383fd621cf0
Max R <max.remkes@gmail.com>
2020-03-01 12:34:25 +0100

    Chapter 3: Web forms improved

--- toto/routes.py
+++ toto/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 
 from toto import app
 from toto.forms import LoginForm
@@ -22,11 +22,13 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
     if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+
         return redirect(url_for('index'))
 
     return render_template('login.html', title='Sign In', form=form)
