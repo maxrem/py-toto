b1627073b2a22d85446a8bc3d10711707c2d4a6d
Max Remkes <max@pbwebmedia.com>
2020-03-02 17:01:29 +0100

    Chapter 7: Error Handling

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## References
 * [Flask tutorial - database](https://flask.palletsprojects.com/en/1.1.x/tutorial/database/)
-* [The Flask Mega - Tutorial - Chapter 7: Error Handling](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vii-error-handling)
+* [The Flask Mega - Tutorial - Chapter 8: Followers](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-viii-followers)
--- toto/__init__.py
+++ toto/__init__.py
@@ -1,3 +1,7 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -20,4 +24,15 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
 
-from toto import routes, models
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from toto import routes, models, errors
